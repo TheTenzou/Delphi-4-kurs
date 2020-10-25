eb02408e2247b579c9863587d8a8df05fce7fe09
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-10-25 14:18:44 +0300

    Test json

--- server/Server.dfm
+++ server/Server.dfm
@@ -11,50 +11,85 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnActivate = FormActivate
   PixelsPerInch = 96
   TextHeight = 13
-  object DBGrid1: TDBGrid
-    Left = 24
-    Top = 8
-    Width = 353
-    Height = 265
-    DataSource = DataSource1
-    TabOrder = 0
-    TitleFont.Charset = DEFAULT_CHARSET
-    TitleFont.Color = clWindowText
-    TitleFont.Height = -11
-    TitleFont.Name = 'Tahoma'
-    TitleFont.Style = []
+  object Label1: TLabel
+    Left = 400
+    Top = 13
+    Width = 25
+    Height = 13
+    Caption = #1055#1086#1088#1090
   end
   object Button1: TButton
-    Left = 472
-    Top = 168
+    Left = 400
+    Top = 272
     Width = 75
     Height = 25
-    Caption = 'Button1'
-    TabOrder = 1
+    Caption = 'bd connection'
+    TabOrder = 0
     OnClick = Button1Click
+  end
+  object Edit1: TEdit
+    Left = 400
+    Top = 32
+    Width = 121
+    Height = 21
+    TabOrder = 1
+    OnChange = Edit1Change
+  end
+  object Button2: TButton
+    Left = 400
+    Top = 72
+    Width = 75
+    Height = 25
+    Caption = #1047#1072#1087#1091#1089#1090#1080#1090#1100
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+  object Button3: TButton
+    Left = 408
+    Top = 144
+    Width = 75
+    Height = 25
+    Caption = 'json'
+    TabOrder = 3
+    OnClick = Button3Click
+  end
+  object RichEdit1: TRichEdit
+    Left = 24
+    Top = 13
+    Width = 337
+    Height = 260
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    Lines.Strings = (
+      'RichEdit1')
+    ParentFont = False
+    TabOrder = 4
+    Zoom = 100
   end
   object FDConnection1: TFDConnection
     Params.Strings = (
       'Database=E:\My_Progs\delphi\Delphi-4-kurs\server\DataBase.db'
       'DriverID=SQLite')
-    Left = 432
-    Top = 32
+    Left = 424
+    Top = 208
   end
   object FDQuery1: TFDQuery
     Connection = FDConnection1
-    Left = 568
-    Top = 32
-  end
-  object FDTransaction1: TFDTransaction
-    Connection = FDConnection1
     Left = 496
-    Top = 32
+    Top = 208
   end
-  object DataSource1: TDataSource
-    DataSet = FDQuery1
+  object ServerSocket1: TServerSocket
+    Active = False
+    Port = 0
+    ServerType = stNonBlocking
+    OnClientRead = ServerSocket1ClientRead
     Left = 568
-    Top = 112
+    Top = 16
   end
 end
--- server/Server.pas
+++ server/Server.pas
@@ -10,17 +10,27 @@ uses
   FireDAC.VCLUI.Wait, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
   FireDAC.DApt, Vcl.StdCtrls, Vcl.Grids, Vcl.DBGrids, FireDAC.Comp.DataSet,
   FireDAC.Comp.Client, FireDAC.Phys.MySQLDef, FireDAC.Phys.MySQL,
-  FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteDef, FireDAC.Phys.SQLite;
+  FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteDef, FireDAC.Phys.SQLite,
+  System.Win.ScktComp, System.JSON, Vcl.ComCtrls;
 
 type
   TForm1 = class(TForm)
-    DBGrid1: TDBGrid;
     Button1: TButton;
     FDConnection1: TFDConnection;
     FDQuery1: TFDQuery;
-    FDTransaction1: TFDTransaction;
-    DataSource1: TDataSource;
+    ServerSocket1: TServerSocket;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Button2: TButton;
+    Button3: TButton;
+    RichEdit1: TRichEdit;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Edit1Change(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+    procedure ServerSocket1ClientRead(Sender: TObject;
+      Socket: TCustomWinSocket);
+    procedure Button3Click(Sender: TObject);
 
   private
     { Private declarations }
@@ -39,11 +49,30 @@ procedure TForm1.Button1Click(Sender: TObject);
 var str,name:String;
     i:Integer;
 begin
-FDQuery1.Active:=False;
-FDQuery1.SQL.Text:='SELECT name, seq FROM sqlite_sequence ;';
-FDQuery1.SQL.Text:='SELECT id, name, price FROM Products;';
-FDQuery1.Active:=True;
-FDQuery1.First;
+
+FDConnection1.Close;
+FDConnection1.Params.Clear;
+FDConnection1.Params.Add('DriverID=SQLite');
+FDConnection1.Params.Add('Database='+ExtractFilePath(Application.ExeName)+'\DataBase.db');
+try
+  FDConnection1.Open;
+  ShowMessage('database open');
+except
+  ShowMessage('nope');
+  exit;
+end;
+
+FDConnection1.StartTransaction;
+try
+  FDQuery1.Active:=False;
+  FDQuery1.SQL.Text:='SELECT name, seq FROM sqlite_sequence ;';
+  FDQuery1.SQL.Text:='SELECT id, name, price FROM Products;';
+  FDQuery1.Active:=True;
+  FDQuery1.First;
+  FDConnection1.Commit;
+except
+  FDConnection1.Rollback;
+end;
 
 ShowMessage(IntToStr(FDQuery1.RowsAffected));
 
@@ -58,6 +87,42 @@ while (not FDQuery1.Eof) do
     ShowMessage(str);
     FDQuery1.Next;
   end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+ServerSocket1.Active:=False;
+ServerSocket1.Port:=StrToInt(Edit1.Text);
+ServerSocket1.Active:=True;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var JsonObject: TJSONObject;
+begin
+  JsonObject:=TJSONObject.Create;
+  JsonObject.AddPair('hello','world');
+  RichEdit1.Lines.Add(JsonObject.Format());
+  ShowMessage(RichEdit1.Lines.Text);
+end;
+
+procedure TForm1.Edit1Change(Sender: TObject);
+begin
+if (Edit1.Text <> '') then
+  Button2.Enabled:=True
+else
+  Button2.Enabled:=False;
+
+end;
+
+procedure TForm1.FormActivate(Sender: TObject);
+begin
+Button2.Enabled:=False;
+end;
+
+procedure TForm1.ServerSocket1ClientRead(Sender: TObject;
+  Socket: TCustomWinSocket);
+begin
+//
 end;
 
 end.
