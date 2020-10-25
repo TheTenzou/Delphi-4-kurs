69f2097de09901a0b68317f64689ec7b6489ae5d
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-10-25 11:52:30 +0300

    some updates

--- test-client/ClientForm.pas
+++ test-client/ClientForm.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.Win.ScktComp, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.Win.ScktComp, Vcl.StdCtrls,
+  IdBaseComponent, IdComponent, IdTCPConnection, IdTCPClient;
 
 type
   TClient = class(TForm)
@@ -23,6 +24,7 @@ type
     Button2: TButton;
     Memo1: TMemo;
     ClientSocket1: TClientSocket;
+    IdTCPClient1: TIdTCPClient;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure ClientSocket1Connect(Sender: TObject; Socket: TCustomWinSocket);
@@ -44,15 +46,18 @@ implementation
 
 procedure TClient.Button1Click(Sender: TObject);
 begin
-ClientSocket1.Active:=False;
-ClientSocket1.Host:=Edit1.Text;
-ClientSocket1.Port:=StrToInt(Edit2.Text);
-ClientSocket1.Active:=True;
+//ClientSocket1.Active:=False;
+//ClientSocket1.Host:=Edit1.Text;
+//ClientSocket1.Port:=StrToInt(Edit2.Text);
+//ClientSocket1.Active:=True;
+IdTCPClient1.Host:=Edit1.Text;
+IdTCPClient1.Port:=StrToInt(Edit2.Text);
 end;
 
 procedure TClient.Button2Click(Sender: TObject);
 begin
-ClientSocket1.Socket.SendText(Edit5.Text);
+//ClientSocket1.Socket.SendText(Edit5.Text);
+//IdTCPClient1.Socket.
 Memo1.Lines.Add('Me : '+Edit5.Text);
 end;
 
