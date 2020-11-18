6dfee61c9fe4c4ce8db16651985e3ebbb78968ef
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-11-18 12:32:34 +0300

    Add connection to database

--- server/ServerUnit.dfm
+++ server/ServerUnit.dfm
@@ -1,9 +1,11 @@
 object ServerForm: TServerForm
   Left = 0
   Top = 0
+  BorderIcons = [biSystemMenu, biMinimize]
+  BorderStyle = bsSingle
   Caption = #1057#1077#1088#1074#1077#1088
-  ClientHeight = 299
-  ClientWidth = 635
+  ClientHeight = 309
+  ClientWidth = 645
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -16,21 +18,21 @@ object ServerForm: TServerForm
   TextHeight = 13
   object PortLabel: TLabel
     Left = 8
-    Top = 24
+    Top = 13
     Width = 25
     Height = 13
     Caption = #1055#1086#1088#1090
   end
   object PortEdit: TEdit
     Left = 56
-    Top = 21
+    Top = 10
     Width = 121
     Height = 21
     TabOrder = 0
   end
   object StartButton: TButton
     Left = 192
-    Top = 19
+    Top = 8
     Width = 97
     Height = 25
     Caption = #1047#1072#1087#1091#1089#1082' '#1089#1077#1088#1074#1077#1088#1072
@@ -39,7 +41,7 @@ object ServerForm: TServerForm
   end
   object StopButton: TButton
     Left = 312
-    Top = 19
+    Top = 8
     Width = 105
     Height = 25
     Caption = #1054#1089#1090#1072#1085#1086#1074#1080#1090' '#1089#1077#1088#1074#1077#1088
@@ -48,16 +50,35 @@ object ServerForm: TServerForm
   end
   object StatusMemo: TMemo
     Left = 0
-    Top = 50
-    Width = 635
-    Height = 249
+    Top = 39
+    Width = 645
+    Height = 270
     Align = alBottom
     Enabled = False
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Courier New'
+    Font.Style = []
+    HideSelection = False
+    ParentFont = False
     TabOrder = 3
   end
   object HTTPServer: TIdHTTPServer
     Bindings = <>
-    Left = 584
-    Top = 16
+    Left = 24
+    Top = 64
+  end
+  object DBConnection: TFDConnection
+    Params.Strings = (
+      'Pooled=True'
+      'DriverID=SQLite')
+    Left = 96
+    Top = 64
+  end
+  object Query: TFDQuery
+    Connection = DBConnection
+    Left = 168
+    Top = 64
   end
 end
--- server/ServerUnit.pas
+++ server/ServerUnit.pas
@@ -6,7 +6,12 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls,
   IdBaseComponent, IdComponent, IdCustomTCPServer, IdCustomHTTPServer,
-  IdHTTPServer;
+  IdHTTPServer, FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error,
+  FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool,
+  FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait, FireDAC.Stan.Param,
+  FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client, FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef,
+  FireDAC.Stan.ExprFuncs;
 
 type
   TServerForm = class(TForm)
@@ -16,6 +21,8 @@ type
     StartButton: TButton;
     StopButton: TButton;
     StatusMemo: TMemo;
+    DBConnection: TFDConnection;
+    Query: TFDQuery;
     procedure StartButtonClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure StopButtonClick(Sender: TObject);
@@ -27,6 +34,7 @@ type
 
 var
   ServerForm: TServerForm;
+  datePadding: integer;
 
 implementation
 
@@ -36,25 +44,59 @@ procedure TServerForm.FormCreate(Sender: TObject);
 begin
 PortEdit.Text := '80';
 StopButton.Enabled := False;
+datePadding := 60;
 end;
 
 procedure TServerForm.StartButtonClick(Sender: TObject);
+var
+  params : TStrings;
+  i : integer;
+  str : string;
 begin
   try
     HTTPServer.DefaultPort := StrToInt(PortEdit.Text);
     HTTPServer.Active := True;
 
-    StatusMemo.Lines.Add('Сервер запушен на порте: '
-                             + PortEdit.Text
-                             + '. Време ' + DateTimeToStr(Now));
+    StatusMemo.Lines.Add(('Сервер запушен на порте: '
+                          + PortEdit.Text + '.').PadRight(datePadding)
+                          + 'Време ' + DateTimeToStr(Now));
 
     StopButton.Enabled := True;
 
     StartButton.Enabled := False;
     PortEdit.Enabled := False;
+
+    params := TStringList.Create;
+    params.Add('DriverID=SQLite');
+    params.Add('Pooled=True');
+    params.Add('Database='
+                + ExtractFilePath(Application.ExeName)
+                + '\DataBase.db');
+
+    try
+      DBConnection.Close;
+      FDManager.AddConnectionDef('sqlitePooled','SQLite', params);
+      DBConnection.ConnectionDefName := 'sqlitePooled';
+      
+      DBConnection.Open;
+      StatusMemo.Lines.Add('Успешное подлючение к базеданных.'.PadRight(datePadding)
+                           + 'Время ' +  DateTimeToStr(Now));
+    except
+      on E : Exception do
+      begin
+        ShowMessage('Соошени ошибки: '+E.Message);
+
+        StatusMemo.Lines.Add('Ошибка подлючения к базеданных.'.PadRight(datePadding)
+                           + 'Время ' +  DateTimeToStr(Now));
+        exit;
+      end;
+    end;
+
   except
-    StatusMemo.Lines.Add('Ошибка запуска сервера');
+    StatusMemo.Lines.Add(('Ошибка запуска сервера.').PadRight(datePadding)
+                          + DateTimeToStr(Now));
   end;
+  
 end;
 
 procedure TServerForm.StopButtonClick(Sender: TObject);
@@ -62,14 +104,16 @@ begin
   try
     HTTPServer.Active := False;
 
-    StatusMemo.Lines.Add('Сервер остановлен. Време ' + DateTimeToStr(Now));
+    StatusMemo.Lines.Add('Сервер остановлен.'.PadRight(datePadding)
+                          + 'Време ' + DateTimeToStr(Now));
 
     StartButton.Enabled := True;
     PortEdit.Enabled := True;
 
     StopButton.Enabled := False;
   except
-     StatusMemo.Lines.Add('Ошибка остановки сервера');
+     StatusMemo.Lines.Add(('Ошибка остановки сервера.').PadRight(datePadding)
+                            + DateTimeToStr(Now));
   end;
 end;
 
