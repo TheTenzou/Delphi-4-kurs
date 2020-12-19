5ec9499373b7c413476ea84b956dc52572bc8f5f
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-12-19 23:59:30 +0300

    imports

--- AndroidClient/app/src/main/java/com/example/androidclient/ui/OrdersActivity.java
+++ AndroidClient/app/src/main/java/com/example/androidclient/ui/OrdersActivity.java
@@ -1,18 +1,10 @@
 package com.example.androidclient.ui;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.provider.SyncStateContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
