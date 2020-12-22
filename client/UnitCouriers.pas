b0da9b5b982e7c701a7574bcb30d0455c7ef9566
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2020-12-23 01:31:27 +0300

    Add couriers form

---
+++ client/UnitCouriers.pas
@@ -0,0 +1,24 @@
+unit UnitCouriers;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus;
+
+type
+  TFormCouriers = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormCouriers: TFormCouriers;
+
+implementation
+
+{$R *.dfm}
+
+end.
